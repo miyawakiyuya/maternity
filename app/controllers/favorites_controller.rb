bbbc93d864e480cd6a77476d1a9f02d13c2b4775
miyawakiyuya <h106y110@gmail.com>
2021-09-21 13:48:27 +0000

    favorite_update2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,14 +1,14 @@
 class FavoritesController < ApplicationController
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_user.favorites.new(post_id: post.id)
-    favorite.save
+    @post = Post.find(params[:post_id])
+    favorite = current_user.favorites.new(post_id: @post.id)
+     favorite.save
     # redirect_to posts_path
   end
 
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = Favorite.find_by(post_id: post.id)
+    @post = Post.find(params[:post_id])
+    favorite = Favorite.find_by(post_id: @post.id,user_id: current_user.id)
     favorite.destroy
     # redirect_to posts_path
   end
